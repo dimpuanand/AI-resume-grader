394fb10d7fc27a1c467b854e6ea10981d7ae3f2c
Anand <dimpuanand01@gmail.com>
2026-06-01 01:17:46 +0530

    Use MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/resumegrader");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
